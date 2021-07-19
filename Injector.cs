cce9878be63b3ac33ab7a9decbc972721c8a16b2
Sudeep <sudeep.rp@gmail.com>
2021-07-19 07:02:24 +0530

    illustrate injection

---
+++ Injector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    interface IManualOutput
+    {
+        void printALine(string line);
+    }
+    class ManualOnConsole: IManualOutput
+    {
+        public void printALine(string line) {
+            Console.WriteLine(line);
+        }
+    }
+    class ManualContentGrabber : IManualOutput
+    {
+        public string heading;
+        public string content;
+        public void printALine(string line) {
+            if (heading == null) {
+                heading = line;
+            } else {
+                content = line;
+            }
+        }
+    }
+    class DependencyInjector
+    {
+        static void PrintManual(IManualOutput destination) {
+            destination.printALine("Color-Pair    Number");
+            destination.printALine("White-Blue       1  ");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------\n* Printing to console");
+            PrintManual(new ManualOnConsole());
+            Console.WriteLine("-------------");
+            Console.WriteLine("* Testing the content");
+            var grabber = new ManualContentGrabber();
+            PrintManual(grabber);
+            Console.WriteLine("Assert the heading here: " + grabber.heading);
+            Console.WriteLine("Assert this content: " + grabber.content);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# inject-for-testing
+# Inject for Testing
+
+One of the exercises in the [Modularity Exercise](https://github.com/clean-code-craft-tcq-1/modular-colorcoder-cs) is to extend functionality. The new functionality is to print the color-code in the form of a reference manual.
+
+The challenge is to write a unit-test for this functionality. The function prints a manual and doesn't return anything.
+
+This repository illustrates an outline of dependency-injection to enable unit-testing of that functionality.
